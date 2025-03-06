09f1dc46548f76ebd1a229529b2f05c86afe4f91
nelsonksh <nelsonksh@gimbalabs.io>
2025-03-06 23:10:51 +0530

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,55 +1,44 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { CardanoQueryClient } from "@utxorpc/sdk";
 import Image from "next/image";
+import { useState } from "react";
+
+async function readParamsExample(port: number) {
+  const queryClient = new CardanoQueryClient({
+    uri: `http://localhost:${port}`,
+    // uri: "https://preprod.utxorpc-v0.demeter.run:443",
+    // headers: {
+    //   "dmtr-api-key": "dmtr_utxorpc15dnupstcsym5xjd7yha0eccta5x6s353",
+    // },
+  });
+
+  const params = await queryClient.readParams();
+  console.log(params);
+}
 
 export default function Home() {
+  const [port, setPort] = useState<number>(8081);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <div className="flex flex-row gap-4">
+          <Label>Port</Label>
+          <Input type="number" placeholder="port" onChange={(e) => setPort(Number(e.target.value))} />
         </div>
+        <div>
+          <Button
+            onClick={() => {
+              readParamsExample(port).catch(console.error);
+            }}
+          >
+            Read Params
+          </Button>
+        </div>
+        
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
